ff48478fbcaea9fd43f32013267597368e32e7c2
msg4edson <msg.edson@gmail.com>
2026-07-09 21:46:15 -0300

    Add main application logic for interactive Kubernetes chat and update requirements

---
+++ main.py
@@ -0,0 +1,57 @@
+from email.mime import message
+import json
+import os
+
+import sh
+from openai import OpenAI
+
+client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+model_name = "gpt-4o"
+tools = [{
+    "type": "function",
+    "function": {
+        "name": "kubectl",
+        "description": "execute kubectl commands against a k8s cluster",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "cmd": {
+                    "type": "string",
+                    "description": "The kubectl command to execute, (without kubectl, just the arguments). For example, 'get pods'"
+                }
+            },
+            "required": ["cmd"]
+        }
+    },
+}]
+
+def send(messages: list[dict[str, any]]) -> str:
+    response = client.chat.completions.create(
+        model=model_name, messages=messages, tools=tools, tool_choice="auto")
+    r = response.choices[0].message
+    if r.tool_calls:
+        message = dict(
+            role=r.role,
+            content=r.content,
+            tool_calls=[dict(id=t.id, type=t.type, function=dict(name=t.function.name, arguments=t.function.arguments)
+                             ) for t in r.tool_calls if t.function])
+        messages.append(message)
+        for t in r.tool_calls:
+            if t.function.name == "kubectl":
+                cmd = json.loads(t.function.arguments)["cmd"].split()
+                result = sh.kubectl(*cmd)
+                messages.append(dict(tool_call_id=t.id, role="tool", name=t.function.name, content=result))
+        return send(messages)
+    return r.content.strip()
+
+def main():
+    print("☸️ Interactive Kubernetes Chat. Type 'exit' to quit.\n" + "-" * 52)
+    messages = [{'role': 'system', 'content': 'You are a Kubernetes expert ready to help'}]
+    while (user_input := input("👤 You: ")).lower() != 'exit':
+        messages.append(dict(role="user", content=user_input))
+        response = send(messages)
+        print(f"🤖 AI: {response}\n----------")
+
+
+if __name__ == "__main__":
+    main()
